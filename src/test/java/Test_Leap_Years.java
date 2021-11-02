266579566030c222ff4cb14a80c975e7924a22da
Thomassmithmathisen <43747006+Thomassmithmathisen@users.noreply.github.com>
2021-11-02 12:12:24 +0100

    Changed name of first test.

--- src/test/java/Test_Leap_Years.java
+++ src/test/java/Test_Leap_Years.java
@@ -6,7 +6,7 @@ public class Test_Leap_Years {
 
 
   @Test
-    public void CheckIfDivBy4AndNot100(){
+    public void TestIfDivBy4AndNot100(){
     boolean leap = YearsCheck.isLeapYear(2020);
     assertTrue(leap);
   }
